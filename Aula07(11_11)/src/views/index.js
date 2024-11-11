3e4ee0a300d420cc70ce6000d6cfc7c633419ed1
GABRIEL DIAS CUNHA LIMA <a92279958@rn.senac.local>
2024-11-11 17:27:08 -0300

    add: adicionei a aula 11/11

---
+++ Aula07(11_11)/src/views/index.js
@@ -0,0 +1,37 @@
+const prompt = require("prompt-sync")()
+const {VeiculoController} = require("../controllers/VeiculoController")
+const controller = new VeiculoController()
+
+function iniciarSistema(){
+    let opcao
+    do{
+        console.log('=== Sistema de Gerenciamentos de Veículos ===')
+        console.log('1. Adicionar Veículo')
+        console.log('2. Listar Veículo')
+        console.log('3. Sair')
+        opcao = prompt('Escolha uma opção:')
+        switch(opcao){
+            case "1":
+                adicionarVeiculo()
+                break
+            case "2":
+                controller.listarVeiculos()
+                break
+            case "3":
+                console.log("Saindo do sistema ...")
+                break
+            default:
+                console.log('Opção Inválida')            
+        }
+    }while(opcao!=="3")
+}
+
+function adicionarVeiculo(){
+    const tipo = prompt ('Digite o tipo de veículo (Carro/Moto/Veículo)')
+    const marca = prompt ('Digite a marca do veículo:')
+    const ano = parseInt(prompt ('Digite o ano do veículo:'))
+    const modelo = prompt ('Digite o modelo do veículo:')
+    const cor = prompt ('Digite a cor do veículo:')
+    controller.criarVeiculo(tipo,marca,ano,cor,modelo)
+}
+iniciarSistema()
