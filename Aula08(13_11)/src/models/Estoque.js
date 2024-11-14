bc2d040dfcd1be0e8a93e0b95dd7327d7839f84e
GABRIEL DIAS CUNHA LIMA <a92279958@rn.senac.local>
2024-11-14 17:08:01 -0300

    add: adicionei a aula do dia 14/11

--- Aula08(13_11)/src/models/Estoque.js
+++ Aula08(13_11)/src/models/Estoque.js
@@ -1,9 +1,23 @@
-class Estoque{
-    quantidade
-    produtos
-    constructor(quantidade,produtos){
-        this.quantidade = quantidade
-        this.produtos = produtos
+const { Notebook } = require('../models/Notebook');
+class Estoque {
+    #id
+    constructor(id, produto, quantidade) {
+        try {
+            if (produto instanceof Notebook) {
+                this.#id = id;
+                this.produto = produto;
+                this.quantidade = quantidade;
+            }
+        }catch(error){
+            console.error('Não é permitido criar Estoque.')
+        } 
     }
+get getId(){
+    return this.#id;
 }
-module.exports = {Estoque}
+set setId(novoId){
+    this.#id = novoId;
+}
+}
+
+module.exports = {Estoque};
